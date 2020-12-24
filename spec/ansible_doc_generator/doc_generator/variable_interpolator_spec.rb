00010cab3912a7a5a314caf7f4ae7188432beb1a
Víctor <victor@populate.tools>
2020-12-24 11:16:00 +0100

    variable interpolator spec

---
+++ spec/ansible_doc_generator/doc_generator/variable_interpolator_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+describe AnsibleDocGenerator::DocGenerator::VariableInterpolator do
+  subject { described_class.new(input, task) }
+
+  describe '#call' do
+    context 'basic interpolation' do
+      let(:input) { 'Add a line in #{lineinfile>destfile} to activate rbenv' }
+      let(:task) do
+        {
+          'name' => 'Activate rbenv in users',
+          'become' => true,
+          'lineinfile' => {
+            'destfile' => '/etc/bashrc',
+            'state' => 'present'
+          }
+        }
+      end
+
+      it 'returns the expected output' do
+        expect(subject.call).to eq 'Add a line in /etc/bashrc to activate rbenv'
+      end
+    end
+
+    context 'interpolation with filters' do
+      let(:input) { 'apt install -y #{apt>pkg | join: \' \'}' }
+      let(:task) do
+        {
+          'name' => 'Install required packages',
+          'apt' => {
+            'pkg' => ["dirmngr", "gnupg", "apt-transport-https", "ca-certificates"]
+          }
+        }
+      end
+
+      it 'returns the expected output' do
+        expect(subject.call).to eq 'apt install -y dirmngr gnupg apt-transport-https ca-certificates'
+      end
+    end
+  end
+end
